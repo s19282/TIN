c1c3a592e31832d08b7fc44e94d4ece41fa312f1
Mateusz <s19282@pjwstk.edu.pl>
2020-11-10 21:55:23 +0100

    added transitions and simple js script

--- _js/script.js
+++ _js/script.js
@@ -1,1 +1,6 @@
-// logo -> onclick( ) -> reverseColor,,,, smooth color change
+function changeFooter()
+{
+    let paragraph = document.getElementsByTagName('footer')[0];
+    paragraph.innerText = "Copyright ©, s19282, Godlewski, Mateusz";
+    setTimeout(function (){paragraph.innerText = "Mateusz, Godlewski, s19282, Copyright ©";},1000);
+}
